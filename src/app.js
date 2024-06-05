0a78fc8e46389ff7f5dde2fb17cb0b0b17bf7d0e
sophiapurple <146739478+sophiapurple@users.noreply.github.com>
2024-06-06 00:14:25 +0100

    integrated axios in the html head

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
+function searchCity(city){
+    let apiKey = "93cf0a589b1befff9b43f05fbt79bo02";
+    let  apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+  
+    axios.get(apiUrl).then(displayWeather);
+}
+
+
 function handleSubmit(event){
     event.preventDefault();
 let cityElement = document.querySelector("#city");
 let cityInput= document.querySelector("#city-input-text");
 cityElement.innerHTML= cityInput.value;
 searchCity(cityInput,value);
-
-
 
 }
 
